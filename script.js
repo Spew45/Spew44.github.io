c1a48fc6b12e30305221eee2ee3da1dd4d2a4a91
Spew45 <103700395+Spew45@users.noreply.github.com>
2022-04-20 20:19:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ let y = 2
 y = y + 2
 let z = x + y
 
+//im not even sure
 console.log(z)
 console.log(y)
 console.log(x)
 
 //idk
 alert("Welcome!");
+alert("Make Sure Your Screen Size Is Perfect By Pressing Ctrl+Shift+-");
+
